79c05e2b37102e181dda57d54ce87d2c2d5c8273
Ubuntu <ubuntu@ip-172-31-8-220.us-east-2.compute.internal>
2025-02-19 03:06:23 +0000

    Add HashMap to store DNS records from file contents

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::env;
 use std::io::{stdin, stdout, Write};
+use std::collections::HashMap;
 
 fn main() {
     let mut file_path = String::new();
@@ -12,5 +13,22 @@ fn main() {
 
     let contents = fs::read_to_string(&file_path).expect("Failed to read file");
 
-    println!("\nContents:\n{contents}");
+    let mut n = 0;
+    let mut dns: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for line in contents.lines() {
+        if !(line.starts_with('#') || line.is_empty()) {
+            println!("#{n}\t| {line}");
+            n += 1;
+            if line.contains(' ') {
+                let mut parts = line.split_whitespace();
+                if let Some(ip) = parts.next() {
+                    let names = parts.collect::<Vec<&str>>();
+                    dns.insert(ip, names);
+                }
+            }
+        }
+    }
+
+    println!("{:?}", dns);
 }
