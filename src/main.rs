9719c85a1ec86bd203d50c429336bc89adf66e03
Ubuntu <ubuntu@ip-172-31-8-220.us-east-2.compute.internal>
2025-02-19 06:11:42 +0000

    Add support for serializing DNS entries to JSON

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,64 @@
 use std::fs;
 use std::collections::HashMap;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Entry<'a> {
+    line_number: usize,
+    names: Vec<&'a str>
+}
+
+impl<'a> Entry<'a> {
+   pub fn new(line_number: usize, names: Vec<&'a str>) -> Self {
+        Entry {
+            line_number,
+            names
+        }
+   }
+}
+
+type Comment<'a> = &'a str;
+
+#[derive(Serialize)]
+enum DNSEntry<'a> {
+    Entry(Entry<'a>),
+    Comment(Comment<'a>),
+    Space
+}
+
+impl <'a> DNSEntry<'a> {
+    pub fn entry(line_number: usize, names: Vec<&'a str>) -> Self {
+        let entry = Entry::new(line_number, names);
+        DNSEntry::Entry(entry)
+    }
+    pub fn comment(comment: &'a str) -> Self {
+        DNSEntry::Comment(comment)
+    }
+
+    pub fn space() -> Self {
+        DNSEntry::Space
+    }
+}
 
 fn main() {
     let file_path = String::from("/etc/hosts");
     let contents = fs::read_to_string(&file_path).expect("Failed to read file");
 
-    let mut dns: HashMap<&str, (usize, Vec<&str>)> = HashMap::new();
+    let mut dns: HashMap<&str, Entry> = HashMap::new();
 
     for (n, line) in contents.lines().enumerate() {
-        if !(line.starts_with('#') || line.is_empty()) {
+        if !(line.is_empty() || line.starts_with('#')) {
             if line.contains(' ') || line.contains('\t') {
                 //println!("line: {line}");
                 let mut parts = line.split_whitespace();
                 if let Some(ip) = parts.next() {
                     let names = parts.collect::<Vec<&str>>();
-                    dns.insert(ip, (n, names));
+                    let entry = Entry::new(n, names);
+                    dns.insert(ip, entry);
                 }
             }
         }
     }
 
-    println!("{:?}", dns);
+    println!("{}", serde_json::to_string_pretty(&dns).expect("Failed to serialize the dns"));
 }
