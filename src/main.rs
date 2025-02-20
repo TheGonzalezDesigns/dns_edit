5535a807110cb72ba22deb56a275fd8c4bc10698
Ubuntu <ubuntu@ip-172-31-8-220.us-east-2.compute.internal>
2025-02-20 03:10:43 +0000

    Update file read and DNS parsing logic in main function

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,24 @@
 use serde::Serialize;
 use std::collections::HashMap;
 use std::fs;
-
-#[derive(Serialize)]
-struct Entry<'a> {
-    line_number: usize,
-    names: Vec<&'a str>,
-}
-
-impl<'a> Entry<'a> {
-    pub fn new(line_number: usize, names: Vec<&'a str>) -> Self {
-        Entry { line_number, names }
-    }
-}
-
-type Comment<'a> = &'a str;
-
-#[derive(Serialize)]
-enum DNSEntry<'a> {
-    Entry(Entry<'a>),
-    Comment(Comment<'a>),
-    Space,
-}
-
-impl<'a> DNSEntry<'a> {
-    pub fn entry(line_number: usize, names: Vec<&'a str>) -> Self {
-        let entry = Entry::new(line_number, names);
-        DNSEntry::Entry(entry)
-    }
-    pub fn comment(comment: &'a str) -> Self {
-        DNSEntry::Comment(comment)
-    }
-
-    pub fn space() -> Self {
-        DNSEntry::Space
-    }
-}
+use std::env;
 
 fn main() {
     let file_path = String::from("/etc/hosts");
-    let contents = fs::read_to_string(&file_path).expect("Failed to read file");
+    let content = fs::read_to_string(&file_path).expect("Failed to read file");
 
-    let mut dns: HashMap<&str, Entry> = HashMap::new();
+    let mut dns: HashMap<&str, &str> = HashMap::new();
 
-    for (n, line) in contents.lines().enumerate() {
+    for (n, line) in content.lines().enumerate() {
         if !(line.is_empty() || line.starts_with('#')) {
             if line.contains(' ') || line.contains('\t') {
                 //println!("line: {line}");
                 let mut parts = line.split_whitespace();
                 if let Some(ip) = parts.next() {
                     let names = parts.collect::<Vec<&str>>();
-                    let entry = Entry::new(n, names);
-                    dns.insert(ip, entry);
+                    for name in names.iter() {
+                        dns.insert(name, ip);
+                    }
                 }
             }
         }
@@ -61,4 +28,11 @@ fn main() {
         "{}",
         serde_json::to_string_pretty(&dns).expect("Failed to serialize the dns")
     );
+
+    let mut raw_args: Vec<String> = env::args().collect::<Vec<String>>();
+    if raw_args.len() > 1 {
+        raw_args.remove(0);
+    }
+    let args = raw_args.join(" ");
+    println!("Args: {args}");
 }
